7525037a95efc6b26ceb9de7b9df6dcf5d01a554
Mrucznik <mrucznix@gmail.com>
2018-10-23 17:45:34 +0200

    Weird style API neo4j with hapi

--- hapi/lib/routes/tables.js
+++ hapi/lib/routes/tables.js
@@ -1,9 +1,24 @@
 'use strict';
 
+const session = require('../../server/neo4j');
+
 module.exports = {
-    method: '',
-    path: '',
+    method: 'GET',
+    path: '/tables',
     options: {
-        handler: async (request, h) => {}
+        handler: (request, h) => {
+            //language=Cypher
+            const query = 'MATCH (tm:ThingMark) RETURN tm';
+
+            session.run(query
+            ).then( (result) => {
+                console.log('gut gut');
+                session.close();
+            }).catch((error) => {
+                console.log(error);
+            });
+
+            return 'kek';
+        }
     }
 };
