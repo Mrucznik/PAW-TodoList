2994cb3846641b8ec637db630213dd5768c69588
Mrucznik <mrucznix@gmail.com>
2018-10-24 22:24:21 +0200

    Hapi mysql

---
+++ hapi-mysql/server/manifest.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const Dotenv = require('dotenv');
+const Confidence = require('confidence');
+const Toys = require('toys');
+
+// Pull .env into process.env
+Dotenv.config({ path: `${__dirname}/.env` });
+
+// Glue manifest as a confidence store
+module.exports = new Confidence.Store({
+    server: {
+        host: '0.0.0.0',
+        port: process.env.PORT || 3000,
+        debug: {
+            $filter: 'NODE_ENV',
+            development: {
+                log: ['error', 'implementation', 'internal'],
+                request: ['error', 'implementation', 'internal']
+            }
+        }
+    },
+    register: {
+        plugins: [
+            {
+                plugin: '../lib', // Main plugin
+                options: {}
+            },
+            {
+                plugin: './plugins/swagger'
+            },
+            {
+                plugin: 'schwifty',
+                options: {
+                    $filter: 'NODE_ENV',
+                    $default: {},
+                    $base: {
+                        migrateOnStart: true,
+                        knex: {
+                            client: 'mysql',
+                            connection: {
+                                host: process.env.DB_HOST,
+                                user: process.env.DB_USER,
+                                password: process.env.DB_PASSWORD,
+                                database: process.env.DB_DATABASE,
+                                charset: process.env.DB_CHARSET
+                            }
+                        }
+                    },
+                    production: {
+                        migrateOnStart: false
+                    }
+                }
+            },
+            {
+                plugin: {
+                    $filter: 'NODE_ENV',
+                    $default: 'hpal-debug',
+                    production: Toys.noop
+                }
+            }
+        ]
+    }
+});
