0385e74f8a28e67b309116e76d5f315dd601612f
KacperMagdziarz <kacper.magdziarz@accenture.com>
2018-11-07 16:03:20 +0100

    fix notifications

--- react/src/App.js
+++ react/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom"
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Notifications from "react-notify-toast";
 
 import BoardsPage from "./PageComponents/BoardsPage";
 import BoardPage from "./PageComponents/BoardPage";
@@ -8,6 +9,8 @@ import Login from "./PageComponents/Login";
 class App extends Component {
     render() {
         return (
+            <div>
+            <Notifications />
             <Router>
                 <div>
                     <Route path={"/"} component={Login}/>
@@ -15,6 +18,7 @@ class App extends Component {
                     <Route path={"/board/:id"} component={BoardPage}/>
                 </div>
             </Router>
+            </div>
         );
     }
 }
